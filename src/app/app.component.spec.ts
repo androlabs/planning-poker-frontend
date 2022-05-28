b731abdacc20d26c4487651d61a6fac28534309c
Anderson Andrad <anderson_andrade_@outlook.com>
2022-05-27 23:12:04 -0300

    chore: configure eslint and settings.json to vscode

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component"
+import { RouterTestingModule } from "@angular/router/testing"
+import { TestBed } from "@angular/core/testing"
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -10,26 +10,26 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent
-      ],
-    }).compileComponents();
-  });
+      ]
+    }).compileComponents()
+  })
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it("should create the app", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
 
-  it(`should have as title 'planning-poker'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('planning-poker');
-  });
+  it("should have as title 'planning-poker'", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app.title).toEqual("planning-poker")
+  })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('planning-poker app is running!');
-  });
-});
+  it("should render title", () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector(".content span")?.textContent).toContain("planning-poker app is running!")
+  })
+})
