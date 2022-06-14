7b50c03c018b6340abeec4c8d559002bd125194a
Anderson Andrad <anderson_andrade_@outlook.com>
2022-06-13 21:31:10 -0300

    feature: create singUpComponent

--- src/app/modules/authenticate/authenticate-routing.module.ts
+++ src/app/modules/authenticate/authenticate-routing.module.ts
@@ -2,11 +2,15 @@ import { RouterModule, Routes } from '@angular/router'
 
 import { NgModule } from '@angular/core'
 import { SingInComponent } from './components/sing-in/sing-in.component'
+import { SingUpComponent } from './components/sing-up/sing-up.component'
 
 const routes: Routes = [
   {
     path: '',
     component: SingInComponent
+  }, {
+    path: 'sing-up',
+    component: SingUpComponent
   }
 ]
 
--- src/app/modules/authenticate/authenticate.module.ts
+++ src/app/modules/authenticate/authenticate.module.ts
@@ -2,11 +2,13 @@ import { AuthenticateRoutingModule } from './authenticate-routing.module'
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { SharedModule } from '../shared/shared.module'
-import { SingInComponent } from './components/sing-in/sing-in.component'
+import { SingInComponent } from './components/sing-in/sing-in.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component'
 
 @NgModule({
   declarations: [
-    SingInComponent
+    SingInComponent,
+    SingUpComponent
   ],
   imports: [
     CommonModule,
