1dbc3017510b128e4a2fc582bb739404234f3984
Anderson Andrad <anderson_andrade_@outlook.com>
2022-06-17 23:23:58 -0300

    feature: create authenticateService

---
+++ src/app/modules/authenticate/authenticate.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SingUp } from './authenticate.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+  private readonly backendUrl = 'http://localhost:3000';
+  constructor(private readonly httpClient: HttpClient) { }
+
+  protected singUp(singUp: SingUp){
+    /* TODO: Implements type observable */
+
+    return this.httpClient.post<any>(`${this.backendUrl}/signup`, {...singUp});
+  }
+
+  protected singIn(singIn: SingUp){
+    /* TODO: Implements type observable */
+
+    return this.httpClient.post<any>(`${this.backendUrl}/login`, {...singIn});
+  }
+
+  protected me(){
+    /* TODO: Implements type observable */
+    return this.httpClient.get<any>(`${this.backendUrl}/me`);
+  }
+
+}
